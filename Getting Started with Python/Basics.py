f64928091cf16288db11a437215a51dea89ae114
Iliano Fasolino <127889824+thatsfaso@users.noreply.github.com>
2023-10-16 23:41:48 +0200

    Update Basics.py

--- Getting Started with Python/Basics.py
+++ Getting Started with Python/Basics.py
@@ -11,3 +11,11 @@ print(x, y, z)
 
 y = range(6)
 print(y)
+
+#trying split method, a method that can pick an entire text and split it in every array index
+
+lista = "mela,banana,kiwi"
+print(lista) #print the whole string
+lista = (lista.split(",")) #now every object of this "lista" is located in an array index
+print(lista[0]) #print mela
+print(lista[1]) #print banana
